79015cd1be17ed92c329f55fbe50f0ababb9f8ae
Jin Kim <jinkim1195@gmail.com>
2020-06-09 15:35:12 -0700

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StackViewExercise
+//
+//  Created by Jin Kim on 6/9/20.
+//  Copyright © 2020 Jin Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
